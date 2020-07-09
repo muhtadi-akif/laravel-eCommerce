c5819a8d13394e4a8a9d3bd39207a15361d248e5
akif <akif@co-well.jp>
2020-07-09 23:57:10 +0600

    customer page header changed

--- resources/views/admin/customer/index.blade.php
+++ resources/views/admin/customer/index.blade.php
@@ -11,7 +11,7 @@
         <div class="container-fluid">
             <div class="row mb-2">
                 <div class="col-sm-6">
-                    <h1>Products</h1>
+                    <h1>Customers</h1>
                 </div>
                 <div class="col-sm-2">
                 </div>
