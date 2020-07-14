7cc7802d0a02c8a4ddfdb9228c866a2cc6a37174
akif <akif@co-well.jp>
2020-07-15 01:49:21 +0600

    order details status constant integers in the model class

--- app/OrderDetail.php
+++ app/OrderDetail.php
@@ -6,10 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class OrderDetail extends Model
 {
-    public function customer(){
+    const STATUS_PENDING = 0;
+    const STATUS_CANCELLED = 1;
+    const STATUS_ACCEPTED = 2;
+
+    public function customer()
+    {
         return $this->belongsTo(Customer::class);
     }
-    public function orders(){
+
+    public function orders()
+    {
         return $this->hasMany(Order::class);
     }
 }
