0d8e1fe9f1ae81ff6c6c4833eee0f9a4f8f7bdef
akif <akif@co-well.jp>
2020-07-19 03:11:39 +0600

    user can post now

--- app/Http/Controllers/Website/PostController.php
+++ app/Http/Controllers/Website/PostController.php
@@ -5,7 +5,9 @@ namespace App\Http\Controllers\Website;
 use App\Category;
 use App\Http\Controllers\Controller;
 use App\Post;
+use Cartalyst\Sentinel\Laravel\Facades\Sentinel;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Redirect;
 
 class PostController extends Controller
 {
@@ -27,7 +29,7 @@ class PostController extends Controller
      */
     public function create()
     {
-        $categories = Category::where('type',Category::TYPE_POST)->get();
+        $categories = Category::where('type', Category::TYPE_POST)->get();
         return view('blog/add', compact('categories'));
     }
 
@@ -39,7 +41,40 @@ class PostController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $user = Sentinel::check();
+        if (!$user) {
+            return Redirect::back()->withErrors("Log in to continue")->withInput($request->input());
+        }
+        request()->validate([
+            'post_image' => 'required|image|mimes:jpeg,png,jpg|max:2048',
+        ]);
+        $title = $request->input('title');
+        $content = $request->input('content');
+        $slug = str_slug($title, '-');
+        $category_id = $request->input('category');
+        if (!$category_id) {
+            return Redirect::back()->withErrors("You have to choose a category")->withInput($request->input());
+        }
+        $post_image = $request->file('post_image');
+        $imageName = $slug . '-' . time() . '.' . $post_image->getClientOriginalExtension();
+        $image_url = $this->getImageUploadRoute() . $imageName;
+
+        $post = new Post();
+        $post->title = $title;
+        $post->slug = $slug;
+        $post->content = $content;
+        $post->image_url = $image_url;
+        $post->category_id = $category_id;
+        $post->customer_id = $user->customer->id;
+        $post->save();
+
+        $post_image->move(public_path($this->getImageUploadRoute()), $imageName);
+        return Redirect::to('/posts');
+    }
+
+    private function getImageUploadRoute()
+    {
+        return '/images/posts/';
     }
 
     /**
--- resources/views/blog/add.blade.php
+++ resources/views/blog/add.blade.php
@@ -5,12 +5,13 @@
             <div class="row">
                 <div class="col-lg-8 col-md-8">
                     <h3 class="mb-30">Add your post here</h3>
-                    <form action="#">
+                    <form action="/posts" method="post" enctype="multipart/form-data">
+                        {{csrf_field()}}
                         <div class="input-group-icon mt-10">
                             <div class="icon"><i class="ti-menu" aria-hidden="true"></i></div>
                             <div class="form-select" id="default-select">
                                 <select name="category">
-                                    <option value="0">Category</option>
+                                    <option value="{{null}}">Category</option>
                                     @foreach($categories as $category)
                                         <option value="{{$category->id}}">{{$category->name}}</option>
                                     @endforeach
@@ -18,19 +19,24 @@
                             </div>
                         </div>
                         <div class="mt-10">
-                            <input type="text" name="first_name" placeholder="Title"
-                                   onfocus="this.placeholder = ''" onblur="this.placeholder = 'Title'" required
+                            <input type="text" name="title" placeholder="Title"
+                                   onfocus="this.placeholder = ''" onblur="this.placeholder = 'Title'" required value="{{ old('title') }}"
                                    class="single-input">
                         </div>
                         <div class="mt-10">
-							<textarea class="single-textarea" placeholder="Content" onfocus="this.placeholder = ''"
-                                      onblur="this.placeholder = 'Content'" required></textarea>
+							<textarea class="single-textarea" name="content" placeholder="Content"
+                                      onfocus="this.placeholder = ''"
+                                      onblur="this.placeholder = 'Content'" required value="{{ old('content') }}"></textarea>
                         </div>
-                        <div class="mt-10">
+                        <div class="mt-10 mb-5">
                             <input type="file"
-                                   class="single-input" id="customFile" accept="image/*" name="product_image"
+                                   class="single-input py-2" id="customFile" accept="image/*" name="post_image"
                                    required>
                             <label class="mt-10" for="customFile">Choose image for the post</label>
+                        </div>
+
+                        <div class="mt-10">
+                            <button type="submit" class="genric-btn primary radius">Submit</button>
                         </div>
                     </form>
                 </div>
