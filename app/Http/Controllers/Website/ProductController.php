29975ac5b4dd0727bff6fa8061529ebd6f456621
akif <akif@co-well.jp>
2020-07-18 21:28:27 +0600

    product page category bug fixed

--- app/Http/Controllers/Website/ProductController.php
+++ app/Http/Controllers/Website/ProductController.php
@@ -22,7 +22,7 @@ class ProductController extends Controller
 
 
         $products = Product::all();
-        $categories = Category::latest()->take(7)->get();
+        $categories = Category::where('type', Category::TYPE_PRODUCT)->latest()->take(7)->get();
         $brands = Brand::latest()->take(7)->get();
         return view('welcome', compact('products', 'categories', 'brands'));
     }
