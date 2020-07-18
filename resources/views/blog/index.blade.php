0596ff4bccac974eedf32a8b326b931089c6b915
akif <akif@co-well.jp>
2020-07-18 21:40:49 +0600

    add a post button will see if only a user is logged in

--- resources/views/blog/index.blade.php
+++ resources/views/blog/index.blade.php
@@ -139,24 +139,17 @@
             </div>
             <div class="col-lg-4">
                 <div class="blog_right_sidebar">
-                    <aside class="single_sidebar_widget search_widget">
-                        <form action="#">
-                            <div class="form-group">
-                                <div class="input-group mb-3">
-                                    <input type="text" class="form-control" placeholder='Search Keyword'
-                                           onfocus="this.placeholder = ''"
-                                           onblur="this.placeholder = 'Search Keyword'">
-                                    <div class="input-group-append">
-                                        <button class="btn" type="button"><i class="ti-search"></i></button>
-                                    </div>
-                                </div>
-                            </div>
-                            <button class="button rounded-0 primary-bg text-white w-100 btn_1"
-                                    type="submit">Search
-                            </button>
-                        </form>
-                    </aside>
-
+                    @if($user = Cartalyst\Sentinel\Laravel\Facades\Sentinel::check())
+                        @if(!$user->hasAccess([App\User::ADMIN_PERMISSION]))
+                            <aside class="single_sidebar_widget search_widget">
+                                <form action="#">
+                                    <button class="button rounded-0 primary-bg text-white w-100 btn_1"
+                                            type="submit">Add a post
+                                    </button>
+                                </form>
+                            </aside>
+                        @endif
+                    @endif
                     <aside class="single_sidebar_widget post_category_widget">
                         <h4 class="widget_title">Category</h4>
                         <ul class="list cat-list">
