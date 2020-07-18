67d40440683cfee769d3441f83c318e2c22bbecb
akif <akif@co-well.jp>
2020-07-18 21:09:36 +0600

    header cleaned and blog page button added

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -57,32 +57,9 @@
                             <li class="nav-item">
                                 <a class="nav-link" href="/">Home</a>
                             </li>
-                            <li class="nav-item dropdown">
-                                <a class="nav-link dropdown-toggle" href="blog.html" id="navbarDropdown_3"
-                                   role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                    pages
-                                </a>
-                                <div class="dropdown-menu" aria-labelledby="navbarDropdown_2">
-                                    <a class="dropdown-item" href="/profile">profile</a>
-                                    <a class="dropdown-item" href="/edit">edit</a>
-                                    <a class="dropdown-item" href="tracking.html">tracking</a>
-                                    <a class="dropdown-item" href="checkout.html">product checkout</a>
-                                    <a class="dropdown-item" href="cart.html">shopping cart</a>
-                                    <a class="dropdown-item" href="confirmation.html">confirmation</a>
-                                    <a class="dropdown-item" href="elements.html">elements</a>
-                                </div>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Blog</a>
                             </li>
-                            <li class="nav-item dropdown">
-                                <a class="nav-link dropdown-toggle" href="blog.html" id="navbarDropdown_2"
-                                   role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                    blog
-                                </a>
-                                <div class="dropdown-menu" aria-labelledby="navbarDropdown_2">
-                                    <a class="dropdown-item" href="blog.html"> blog</a>
-                                    <a class="dropdown-item" href="single-blog.html">Single blog</a>
-                                </div>
-                            </li>
-
                             <li class="nav-item">
                                 @if($user = Cartalyst\Sentinel\Laravel\Facades\Sentinel::check())
                                     @if(!$user->hasAccess([App\User::ADMIN_PERMISSION]))
