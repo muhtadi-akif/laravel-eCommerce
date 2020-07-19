fde914a270c3dca451b05f748d73605179480004
akif <akif@co-well.jp>
2020-07-19 22:08:17 +0600

    home page has now pagination

--- app/Http/Controllers/Website/ProductController.php
+++ app/Http/Controllers/Website/ProductController.php
@@ -21,7 +21,7 @@ class ProductController extends Controller
     {
 
 
-        $products = Product::latest()->paginate(1);
+        $products = Product::latest()->paginate(12);
         $categories = Category::where('type', Category::TYPE_PRODUCT)->latest()->take(7)->get();
         $brands = Brand::latest()->take(7)->get();
         return view('welcome', compact('products', 'categories', 'brands'));
