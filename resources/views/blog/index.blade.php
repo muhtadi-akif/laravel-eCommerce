1f67e4de7b9d8bbd2cc67135dc0a1c8c6e4c4718
akif <akif@co-well.jp>
2020-07-19 21:50:18 +0600

    post page has pagination now

--- resources/views/blog/index.blade.php
+++ resources/views/blog/index.blade.php
@@ -28,22 +28,7 @@
 
                     <nav class="blog-pagination justify-content-center d-flex">
                         <ul class="pagination">
-                            <li class="page-item">
-                                <a href="#" class="page-link" aria-label="Previous">
-                                    <i class="ti-angle-left"></i>
-                                </a>
-                            </li>
-                            <li class="page-item">
-                                <a href="#" class="page-link">1</a>
-                            </li>
-                            <li class="page-item active">
-                                <a href="#" class="page-link">2</a>
-                            </li>
-                            <li class="page-item">
-                                <a href="#" class="page-link" aria-label="Next">
-                                    <i class="ti-angle-right"></i>
-                                </a>
-                            </li>
+                            {{ $posts->links() }}
                         </ul>
                     </nav>
                 </div>
